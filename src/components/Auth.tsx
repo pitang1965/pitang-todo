951d11d36268064a0f9d20da802546676eac5fda
Shoji Makino <pitang1965@gmail.com>
2022-07-17 01:35:13 +0900

    Try to fix: react is not defined

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -20,33 +20,35 @@ export default function Auth() {
   };
 
   return (
-    <div className='row flex flex-center'>
-      <div className='col-6 form-widget'>
-        <h1 className='header'>ピータンTO-DO</h1>
-        <p className='description'>下記Eメールでマジックリンクでログイン</p>
-        <div>
-          <input
-            className='inputField'
-            type='email'
-            placeholder='Eメール'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              handleLogin(email);
-            }}
-            className='button block'
-            disabled={loading}
-          >
-            <span>{loading ? '読み込み中...' : 'マジックリンクを送信'}</span>
-          </button>
+    <>
+      <div className='row flex flex-center'>
+        <div className='col-6 form-widget'>
+          <h1 className='header'>ピータンTO-DO</h1>
+          <p className='description'>下記Eメールでマジックリンクでログイン</p>
+          <div>
+            <input
+              className='inputField'
+              type='email'
+              placeholder='Eメール'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                handleLogin(email);
+              }}
+              className='button block'
+              disabled={loading}
+            >
+              <span>{loading ? '読み込み中...' : 'マジックリンクを送信'}</span>
+            </button>
+          </div>
         </div>
       </div>
       <NotifyContainer />
-    </div>
+    </>
   );
 }
