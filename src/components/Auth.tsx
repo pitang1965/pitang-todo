1c648d027bfc4d2a76350a4af08629384bffe269
Shoji Makino <pitang1965@gmail.com>
2022-07-17 15:28:07 +0900

    Revert Auth.tsx

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { supabase } from '../utils/supabaseClient';
-import { alertApiError, notifyInfo, NotifyContainer } from '../utils/notify';
 
 export default function Auth() {
   const [loading, setLoading] = useState(false);
@@ -11,44 +10,41 @@ export default function Auth() {
       setLoading(true);
       const { error } = await supabase.auth.signIn({ email });
       if (error) throw error;
+      alert('リンクのためのメールをご確認ください。');
     } catch (error: any) {
-      alertApiError(error.error_description || error.message);
+      alert(error.error_description || error.message);
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <>
-      <div className='row flex flex-center'>
-        <div className='col-6 form-widget'>
-          <h1 className='header'>ピータンTO-DO</h1>
-          <p className='description'>下記Eメールでマジックリンクでログイン</p>
-          <div>
-            <input
-              className='inputField'
-              type='email'
-              placeholder='Eメール'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div>
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                handleLogin(email);
-                notifyInfo('リンクのためのメールをご確認ください。');
-              }}
-              className='button block'
-              disabled={loading}
-            >
-              <span>{loading ? '読み込み中...' : 'マジックリンクを送信'}</span>
-            </button>
-          </div>
+    <div className='row flex flex-center'>
+      <div className='col-6 form-widget'>
+        <h1 className='header'>ピータンTO-DO</h1>
+        <p className='description'>下記Eメールでマジックリンクでログイン</p>
+        <div>
+          <input
+            className='inputField'
+            type='email'
+            placeholder='Eメール'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              handleLogin(email);
+            }}
+            className='button block'
+            disabled={loading}
+          >
+            <span>{loading ? '読み込み中...' : 'マジックリンクを送信'}</span>
+          </button>
         </div>
       </div>
-      <NotifyContainer />
-    </>
+    </div>
   );
 }
