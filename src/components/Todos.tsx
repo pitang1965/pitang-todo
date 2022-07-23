c70ecc29dbb584b9801ca89ffb33d3ed55b9d497
Shoji Makino <pitang1965@gmail.com>
2022-07-23 10:49:51 +0900

    Add lottie animation

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,31 +1,98 @@
-import { useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { supabase } from '../utils/supabaseClient';
 import { Session } from '@supabase/gotrue-js';
 import { useSnackbar } from 'notistack';
 import type { SnackbarMessage } from 'notistack';
 import TodoCard from './TodoCard';
 import type { Todo } from './TodoCard';
+import { Player, PlayerEvent } from '@lottiefiles/react-lottie-player';
 
 type Props = {
   session: Session;
 };
 
+function waitAsync(
+  conditionCallback: () => boolean,
+  intervalMillSecond = 10,
+  timeoutMillSecond = 0
+) {
+  // 条件が成立するまで setInterval でポーリング的なループ
+  return new Promise(function (resolve, reject) {
+    let loopCount = 0;
+    const intervalId = setInterval(function () {
+      if (
+        timeoutMillSecond > 0 &&
+        loopCount * intervalMillSecond > timeoutMillSecond
+      ) {
+        reject('timeout'); // 条件が満たされないままタイムアウトを迎えたことを示す
+      }
+      if (!conditionCallback()) {
+        // 条件関数が falsy を返した時はループ続行
+        return;
+      }
+      // 条件関数が truthy を返した時はループ用の interval を消去
+      clearInterval(intervalId);
+      // 条件関数が true を返した時は resolve 関数を実行して条件が満たされたことを示す
+      resolve('success');
+    }, intervalMillSecond);
+  });
+}
+
 export default function Todos({ session }: Props) {
-  const [loading, setLoading] = useState(true);
   const [todos, setTodos] = useState<Todo[]>([]);
   const [newTask, setNewTask] = useState('');
   const { enqueueSnackbar } = useSnackbar();
 
   useEffect(() => {
-    getTodos();
+    (async () => await getTodos())();
   }, [session]);
+
+  useEffect(() => playAnimation(), [todos]);
+
+  const playerRef = useRef<Player>(null);
+  const isPlayingRef = useRef<boolean>(false);
+
+  const playAnimation = () => {
+    isPlayingRef.current = true;
+    playerRef.current?.setSeeker(0);
+    playerRef.current?.play();
+    waitAsync(() => !isPlayingRef.current, 100, 3000)
+      .then(() => updateAnimationSeeker())
+      .catch((e: any) => console.error(e.message));
+  };
+
+  const handleEventPlayer = (e: PlayerEvent) => {
+    if (e === 'complete') {
+      if (isPlayingRef.current) {
+        isPlayingRef.current = false;
+      }
+    }
+  };
+
+  // アニメーション停止時のフレーム位置を決定
+  const updateAnimationSeeker = () => {
+    // アニメーションをtodoがあるときのフレーム位置にする
+    const setAnimationTodoExist = () => {
+      playerRef.current?.setSeeker(40);
+    };
+
+    // アニメーションをtodoがないときのフレーム位置にする
+    const setAnimationTodoNotExist = () => {
+      playerRef.current?.setSeeker(0);
+    };
+
+    if (todos.length > 0) {
+      setAnimationTodoExist();
+    } else {
+      setAnimationTodoNotExist();
+    }
+  };
 
   async function deleteTask(id: number) {
     if (!session) {
       return;
     }
     try {
-      setLoading(true);
       const newTodos: Todo[] = todos.filter((todo) => todo.id !== id);
 
       const { data, error } = await supabase
@@ -39,8 +106,6 @@ export default function Todos({ session }: Props) {
       setTodos(newTodos);
     } catch (error) {
       enqueueSnackbar(error as SnackbarMessage, { variant: 'error' });
-    } finally {
-      setLoading(false);
     }
   }
 
@@ -49,7 +114,6 @@ export default function Todos({ session }: Props) {
       return;
     }
     try {
-      setLoading(true);
       let newTodo: Todo | undefined;
       const newTodos: Todo[] = todos.map((todo) => {
         if (todo.id !== id) {
@@ -73,8 +137,6 @@ export default function Todos({ session }: Props) {
       }
     } catch (error) {
       enqueueSnackbar(error as SnackbarMessage, { variant: 'error' });
-    } finally {
-      setLoading(false);
     }
   }
 
@@ -83,7 +145,6 @@ export default function Todos({ session }: Props) {
       return;
     }
     try {
-      setLoading(true);
       const { data, error } = await supabase
         .from<Todo>('todos')
         .select('*')
@@ -96,8 +157,6 @@ export default function Todos({ session }: Props) {
       setTodos(data);
     } catch (error) {
       enqueueSnackbar(error as SnackbarMessage, { variant: 'error' });
-    } finally {
-      setLoading(false);
     }
   }
 
@@ -110,23 +169,26 @@ export default function Todos({ session }: Props) {
       return;
     }
     try {
-      setLoading(true);
       const { data, error } = await supabase
         .from<Todo>('todos')
         .insert({ task: newTask, user_id: session.user!.id })
         .single();
-      setTodos([...todos, data!]);
       setNewTask('');
+      setTodos([...todos, data!]);
     } catch (error) {
       enqueueSnackbar(error as SnackbarMessage, { variant: 'error' });
-    } finally {
-      setLoading(false);
     }
   };
 
   return (
     <>
-      <h1>やること</h1>
+      <Player
+        src='https://assets8.lottiefiles.com/packages/lf20_z4cshyhf.json'
+        ref={playerRef}
+        style={{ width: '30vw' }}
+        autoplay
+        onEvent={handleEventPlayer}
+      />
       <div className='flex'>
         <input
           type='text'
